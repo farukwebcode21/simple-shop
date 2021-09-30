abac7a0c35cf0da88525b8a607172adb830ebd20
farukwebcode21 <farukahmadmk@yahoo.com>
2021-09-30 21:22:10 +0600

    colam is working and picture show is working perfectly

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ const loadProducts = () => {
 };
 
 
+
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    // console.log(product.image);
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
